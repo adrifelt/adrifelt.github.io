36922af568ad4ec1c809a5a9056336a12f752d88
APF <felt@google.com>
2014-11-15 09:30:18 -0800

    adding failure demo

---
+++ failure-demo.js
@@ -0,0 +1,23 @@
+function $(element) {
+  return document.getElementById(element);
+}
+
+function successCallback(mediaStream) {
+  $('record-button').classList.remove('faded');
+  console.log('success');
+}
+
+function errorCallback(err) {
+  $('record-button').classList.add('faded');
+  console.log(err);
+}
+
+function buttonClick() {
+  navigator.webkitGetUserMedia({audio: true}, successCallback, errorCallback);
+}
+
+function setup() {
+  $('record-button').addEventListener('click', buttonClick);
+}
+
+document.addEventListener('DOMContentLoaded', setup);
