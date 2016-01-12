b0fc624a76c5fd0c699831956984e7ae5ba88aac
felt <felt@chromium.org>
2016-01-12 12:41:58 -0800

    made the callbackWatcher more robust

--- demos/measurement/track-permission-state.js
+++ demos/measurement/track-permission-state.js
@@ -1,8 +1,17 @@
 // Copyright 2016 Google. All rights reserved.
+// Author: felt@chromium.org
 
-function $(elementName) {
-  return document.getElementById(elementName);
-}
+// This demo illustrates how to gather geolocation permission analytics. It uses
+// two methods so that you can compare how well they work:
+//
+// (1) The callbackWatcher uses the geolocation error/success callbacks and
+//     their values to determine what the permission state is.
+// (2) The apiWatcher additionally uses the Permissions API, which is available
+//     in newer versions of Chrome and Firefox. It can identify more states than
+//     the callbackWatcher.
+//
+// The requestHandler drives the demo by invoking the geolocation API and
+// passing the callback results on to the apiWatcher and callbackWatcher.
 
 // *****************************************************************************
 // PERMISSIONS API WATCHER
@@ -15,8 +24,8 @@ apiWatcher.requestAvailable_ = false;
 apiWatcher.revokeAvailable_ = false;
 
 /**
- * Check whether the permissions API is available, and update the UI
- * accordingly.
+ * Check whether the permissions API is available, and toggle the available
+ * features accordingly.
  */
 apiWatcher.compatCheck = function() {
   if (navigator.permissions) {
@@ -43,55 +52,91 @@ document.addEventListener('DOMContentLoaded', apiWatcher.compatCheck);
 
 // *****************************************************************************
 // CALLBACK WATCHER
-// These methods track the permission state using geolocation callbacks.
+// These methods track the permission state using geolocation callbacks and
+// callback error codes. It relies on timing to differentiate between an
+// automated response (without showing the user a prompt) and an actual user
+// response to a dialog.
 // *****************************************************************************
 
 var callbackWatcher = callbackWatcher || {};
 
 callbackWatcher.Status = {
-  UNKNOWN: 0,
-  REQUESTED: 1,
-  USER_GRANTED: 2,
-  USER_DENIED: 3,
-  AUTO_GRANTED: 4,
-  AUTO_DENIED: 5,
+  UNKNOWN: 0,         // Don't know the permission state.
+  REQUESTED: 1,       // Geolocation API invoked.
+  USER_GRANTED: 2,    // User granted the permission via a dialog.
+  USER_DENIED: 3,     // User denied the permission via a dialog.
+  AUTO_GRANTED: 4,    // Browser automatically granted the permission.
+  AUTO_DENIED: 5,     // Browser automatically denied the permission.
+  FAST_NAVIGATE: 6,   // Navigated away from the page shortly after request.
+  SLOW_NAVIGATE: 7,   // Navigated away from the page without a response.
 };
 
+// Used to differentiate between an automated and human response to a dialog.
 callbackWatcher.THRESHOLD = 5;  // Milliseconds
 callbackWatcher.timestamp_ = 0;
 
-callbackWatcher.countSuccess = function() {
-  console.log("Callback tracking: user granted permission");
-  uiHelper.setCallbackStatus(callbackWatcher.Status.USER_GRANTED);
+// Used to identify situations where the user navigates the page without
+// responding to the dialog.
+callbackWatcher.pending_ = false;
+
+/**
+ * Invoked as part of the geolocation success callback. Record the success, and
+ * compare it to the user-response timing threshold.
+ */
+callbackWatcher.successCallback = function() {
+  var delta = Date.now() - callbackWatcher.timestamp_;
+  if (delta > callbackWatcher.THRESHOLD)
+    uiHelper.recordCallbackStatus(callbackWatcher.Status.USER_GRANTED, delta);
+  else
+    uiHelper.recordCallbackStatus(callbackWatcher.Status.AUTO_GRANTED, delta);
+
+  callbackWatcher.timestamp_ = 0;
+  callbackWatcher.pending_ = false;
 }
 
 /**
- * Record the failure, and check to see whether it was above or below the
- * user-response timing threshold.
+ * Invoked as part of the geolocation error callback. Record the failure, and
+ * compare it to the user-response timing threshold.
  */
-callbackWatcher.countFailure = function() {
+callbackWatcher.failureCallback = function() {
   var delta = Date.now() - callbackWatcher.timestamp_;
-  if (delta > callbackWatcher.THRESHOLD) {
-    uiHelper.setCallbackStatus(callbackWatcher.Status.USER_DENIED);
-    console.log("Callback tracking: user denied permission (" + delta + "ms)");
-  } else {
-    uiHelper.setCallbackStatus(callbackWatcher.Status.AUTO_DENIED);
-    console.log("Callback tracking: browser auto-denied permission (" +
-                delta + "ms)");
-  }
-  timestamp_ = 0;
+  if (delta > callbackWatcher.THRESHOLD)
+    uiHelper.recordCallbackStatus(callbackWatcher.Status.USER_DENIED, delta);
+  else
+    uiHelper.recordCallbackStatus(callbackWatcher.Status.AUTO_DENIED, delta);
+
+  callbackWatcher.timestamp_ = 0;
+  callbackWatcher.pending_ = false;
 }
 
-callbackWatcher.countInvocation = function() {
-  console.log("Callback tracking: requesting permission");
-  uiHelper.setCallbackStatus(callbackWatcher.Status.REQUESTED);
+/**
+ * Invoked before the requestHandler tries to invoke the geolocation function.
+ */
+callbackWatcher.recordInvocation = function() {
+  uiHelper.recordCallbackStatus(callbackWatcher.Status.REQUESTED);
   callbackWatcher.timestamp_ = Date.now();
+  callbackWatcher.pending_ = true;
 }
 
+/**
+ * We can't know the initial status until actually trying to invoke the
+ * geolocation function, so set the UI to initially say "unknown".
+ */
 callbackWatcher.setup = function() {
-  uiHelper.setCallbackStatus(callbackWatcher.Status.UNKNOWN);
+  uiHelper.recordCallbackStatus(callbackWatcher.Status.UNKNOWN);
 }
 document.addEventListener('DOMContentLoaded', callbackWatcher.setup);
+
+callbackWatcher.checkBeforeNavigate = function() {
+  if (!callbackWatcher.pending_)
+    return;
+
+  var delta = Date.now() - callbackWatcher.timestamp_;
+  if (delta > callbackWatcher.THRESHOLD)
+    uiHelper.recordCallbackStatus(callbackWatcher.Status.FAST_NAVIGATE, delta);
+  else
+    uiHelper.recordCallbackStatus(callbackWathcer.Status.SLOW_NAVIGATE, delta);
+}
 
 // *****************************************************************************
 // REQUEST HANDLER
@@ -109,14 +154,14 @@ requestHandler.STORAGE_CLICK = "click";
  * A wrapper to invoke both the apiWatcher and callbackWatcher callbacks.
  */
 requestHandler.successCallback = function() {
-  callbackWatcher.countSuccess();
+  callbackWatcher.successCallback();
 }
 
 /**
  * A wrapper to invoke both the apiWatcher and callbackWatcher callbacks.
  */
 requestHandler.failureCallback = function() {
-  callbackWatcher.countFailure();
+  callbackWatcher.failureCallback();
 }
 
 /**
@@ -124,10 +169,12 @@ requestHandler.failureCallback = function() {
  * geolocation API.
  */
 requestHandler.notifyInvocation = function() {
-  callbackWatcher.countInvocation();
+  callbackWatcher.recordInvocation();
 }
 
-// Try to read geolocation.
+/**
+ * Try to read geolocation. This will trigger a permission check.
+ */
 requestHandler.initiateRequest = function() {
   requestHandler.notifyInvocation();
   navigator.geolocation.getCurrentPosition(
@@ -168,7 +215,7 @@ document.addEventListener('DOMContentLoaded', requestHandler.setup);
 
 // *****************************************************************************
 // UI HELPER
-// The UI helper provides utility methods.
+// The UI helper provides utility methods for displaying the permission status.
 // *****************************************************************************
 
 var uiHelper = uiHelper || {};
@@ -178,10 +225,13 @@ uiHelper.setPermissionStatus = function(message) {
 }
 
 /**
- * Convert the callback status to a human-readable string, and display it.
- * @param(callbackWatcher.Status) newStatus The desired status.
+ * If this were real code, this would store the values and then ping the
+ * analytics server with the user's responses on page unload. Since this is a
+ * demo, instead we display the response on the UI and in the console.
+ * @param {callbackWatcher.Status} newStatus The desired status.
+ * @param {number=} delta The time that passed (optional).
  */
-uiHelper.setCallbackStatus = function(newStatus) {
+uiHelper.recordCallbackStatus = function(newStatus, delta) {
   var humanString = "unavailable";
   if (newStatus == callbackWatcher.Status.UNKNOWN)
     humanString = "unknown";
@@ -195,8 +245,16 @@ uiHelper.setCallbackStatus = function(newStatus) {
     humanString = "auto granted";
   else if (newStatus == callbackWatcher.Status.AUTO_DENIED)
     humanString = "auto denied";
+  else if (newStatus == callbackWatcher.Status.FAST_NAVIGATE)
+    humanString = "navigated too quickly for responding";
+  else if (newStatus == callbackWatcher.Status.SLOW_NAVIGATE)
+    humanString = "navigated without responding";
+
+  var deltaString = "";
+  if (delta) deltaString = " (" + delta + "ms)";
 
   $('status-callbacks').innerText = humanString;
+  console.log("Callback tracking: " + humanString + deltaString);
 }
 
 /**
@@ -212,4 +270,8 @@ uiHelper.displayFeatureStatus = function(elem, supported) {
     elem.innerText = 'unsupported';
     elem.classList.add('feature-unsupported');
   }
+}
+
+function $(elementName) {
+  return document.getElementById(elementName);
 }
